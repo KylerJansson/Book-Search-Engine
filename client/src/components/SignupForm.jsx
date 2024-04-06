1b216531ab5f09994bb330e5a807f9e1c8467afa
Kyler Jansson <kylerjansson@gmail.com>
2024-04-05 18:00:32 -0600

    more changes for testing

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
 import { ADD_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
-import { useMutation } from '@apollo/client';
+
 
 const SignupForm = () => {
   // set initial form state
@@ -12,15 +13,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [addUser, { error }] = useMutation(ADD_USER);
-
-  useEffect(() => {
-    if (error) {
-      setShowAlert(true);
-    } else {
-      setShowAlert(false);
-    }
-  }, [error]);
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -42,9 +35,7 @@ const SignupForm = () => {
         variables: { ...userFormData },
       });
 
-      const { token, user } = data.addUser;
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
